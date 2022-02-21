70d59f5aa95cfeb30624fc4d257cf7292a691f10
xuehaoyu <1169732429@qq.com>
2022-02-21 12:29:10 +0800

    update .gitignore

--- src/scan/index.ts
+++ src/scan/index.ts
@@ -1,5 +1,5 @@
 
-import Camera from './camera';
+import Camera from './camera'
 
 interface CameraOptions {
   width: number,
@@ -9,47 +9,47 @@ interface CameraOptions {
 }
 
 export default class ScanClient {
-  private static bleAddress: string;
-  private types: string[];
-  private cameraOptions: CameraOptions;
-  private promise: any;
-  private timeout: any;
+  private static bleAddress: string
+  private types: string[]
+  private cameraOptions: CameraOptions
+  private promise: any
+  private timeout: any
 
   /**
    * 扫码初始化
    * @param errorNotify 错误回调
    * @param types 指定扫码枪类型
    */
-  constructor(bleAddress?: string, cameraOptions?: CameraOptions, types?: string[]) {
+  constructor (bleAddress?: string, cameraOptions?: CameraOptions, types?: string[]) {
     if (!types || !types.length) {
-      types = ['camera'];
+      types = ['camera']
     }
-    ScanClient.bleAddress = bleAddress || ScanClient.bleAddress || '';
-    this.types = types;
-    this.cameraOptions = cameraOptions as any;
+    ScanClient.bleAddress = bleAddress || ScanClient.bleAddress || ''
+    this.types = types
+    this.cameraOptions = cameraOptions as any
   }
 
   /**
    * 扫码
    */
-  scan(errorNotify?: (str: string, res: any) => void): Promise<{ data: string, type: 'bluetooth' | 'camera' | 'hid' } | undefined> {
+  scan (errorNotify?: (str: string, res: any) => void): Promise<{ data: string, type: 'bluetooth' | 'camera' | 'hid' } | undefined> {
     return new Promise((resolve, reject) => {
-      this.promise = { resolve, reject };
-      this.timeout = setTimeout(() => reject(new Error('scan timeout')), 300000); // 5分钟扫描超时
+      this.promise = { resolve, reject }
+      this.timeout = setTimeout(() => reject(new Error('scan timeout')), 300000) // 5分钟扫描超时
       if (~this.types.indexOf('camera')) {
-        Camera.scan(this.cameraOptions, resolve, reject, errorNotify);
+        Camera.scan(this.cameraOptions, resolve, reject, errorNotify)
       }
-    });
+    })
   }
 
   /**
    * 销毁
    */
-  destroy(): void {
-    this.promise && this.promise.resolve('');
-    clearTimeout(this.timeout);
+  destroy (): void {
+    this.promise && this.promise.resolve('')
+    clearTimeout(this.timeout)
     if (~this.types.indexOf('camera')) {
-      Camera.destroy();
+      Camera.destroy()
     }
   }
 }
